bbadbfa73979095747ceeac8baca24d564860b42
GiaMinh20 <nngiaminh@gmail.com>
2022-09-11 00:30:49 +0700

    commit

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d4988558-ed33-4587-a8d0-628d07dc7287")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
